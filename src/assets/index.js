6bf209324b8c941ac9d6321c4cdb26858a35184f
odhisLuka <luketrevor03@gmail.com>
2023-12-20 12:42:27 +0300

    login page

---
+++ src/assets/index.js
@@ -0,0 +1,3 @@
+export { default as loginBg } from "./img/loginBg.jpg";
+export { default as avatar } from "./img/avatar.png";
+export { default as chef1 } from "./img/chef1.png";
--- src/containers/Login.jsx
+++ src/containers/Login.jsx
@@ -1,8 +1,34 @@
 import React from 'react'
+import { loginBg, chef1 } from '../assets'
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <div className='w-screen h-screen relative overflow-hidden flex'>
+      {/* background image */}
+      <img
+        src={loginBg}
+        className='w-full h-full object-cover absolute top-0 left-0'
+        alt=''
+      />
+      {/* content box */}
+      <div className='flex flex-col items-center bg-cardOverlay w-[80%] md:w-508 h-full z-10 backdrop-blur-md p-4 px-4 py-12'>
+        {/* top logo section */}
+        <div className='flex items-center justify-start w-full h-10 gap-3'>
+          <img src={chef1} className='w-16' alt='' />
+          <h1 className='text-headingColor font-semibold text-2xl'>City</h1>
+        </div>
+
+        {/* welcome text */}
+        <div className='flex flex-col items-center justify-center'>
+          <p className='text-headingColor font-semibold text-3xl'>
+            Welcome Back
+          </p>
+          <p className='text-textColor text-xl mb-5'>
+            Sign-in with the following
+          </p>
+        </div>
+      </div>
+    </div>
   )
 }
 
